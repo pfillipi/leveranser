224f1a2364e18e776c4426627bd2cea765b8aec1
Fillipi Pereira <pfillipi@gmx.de>
2024-02-03 05:38:26 +0100

    nativewind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/app/**/*.{ts,tsx}"],
+  content: ["./src/app/**/*.{ts,tsx}", "./src/components/**/*.{ts,tsx}"],
   theme: {
     extend: {},
   },
